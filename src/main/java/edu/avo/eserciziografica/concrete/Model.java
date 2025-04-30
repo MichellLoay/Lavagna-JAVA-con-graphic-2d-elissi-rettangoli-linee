b1a1458f791bb9e9626a780fb12403a09f834da0
shwak <shwak@host-003.homenet.telecomitalia.it>
2025-04-30 20:59:21 +0200

    simulazione di lavagna grafica nella quale vengono illustrate tre immagini elisse, rettangolo, e retta in modo, dimensione, forma,colore,ripieno random con eventuale miglioramento,le figure rispettano i limiti della lavagna cosa che in precedenza non facevano

--- src/main/java/edu/avo/eserciziografica/concrete/Model.java
+++ src/main/java/edu/avo/eserciziografica/concrete/Model.java
@@ -55,10 +55,11 @@ public class Model {
             random.nextInt(256), //G
             random.nextInt(256), //B
         };
-        int x = random.nextInt(width);
-        int y = random.nextInt(height);
+        
         int w = 60 + random.nextInt(100);//dimensiona massima
         int h = 60 + random.nextInt(100);
+        int x = random.nextInt(width - w);//questo erche sia al interno della schermata
+        int y = random.nextInt(height - h);
 
         shapes.add(new Shape(type, fill, color, x, y, w, h));
         observer.update(shapes);
